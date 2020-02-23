9e532dec4eab0b787d7fb2394c0b05d65af91295
Robert <robertgz@users.noreply.github.com>
2020-02-22 22:50:42 -0800

    imported resources

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "@/assets/global.css"
+import "@/lib.js"
 
 Vue.config.productionTip = false
 
