d32cedf13b9281d4fa3aa77709fedd306d14193e
Robert <robertgz@users.noreply.github.com>
2020-02-22 23:37:03 -0800

    added GTM

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGtm from 'vue-gtm';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@/assets/global.css"
 import "@/lib.js"
 
 Vue.config.productionTip = false
+
+// Vue.use(VueGtm, {
+//   id: 'GTM-PLH79B',
+//   enabled: true,
+//   debug: true,
+//   loadScript: true,
+// });
 
 new Vue({
   render: h => h(App),
